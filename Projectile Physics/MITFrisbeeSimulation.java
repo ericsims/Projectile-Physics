e0e0e300362d0f0f95d9dfe79f9b86734a6c5f3e
ericsims <EricSimsM@gmail.com>
2013-06-14 11:02:04 -0400

    Fixed MIT Simulation

--- Projectile Physics/MITFrisbeeSimulation.java
+++ Projectile Physics/MITFrisbeeSimulation.java
@@ -35,69 +35,74 @@ public class MITFrisbeeSimulation {
 	//The drag coefficent at alpha = 0.
 	private static final double CDA = 2.72;
 	//The drag coefficient dependent on alpha.
-	private static final double ALPHA0 = 0;
-	
+	private static final double ALPHA0 = -4;
+
 	public static void main(String args[]) {
-		simulate(1, 10, 20, ALPHA0, 0.00001);
+		simulate(1, 10, 10, 1, 0.0001);
 	}
+
 	
-	/**
-	 * A method that uses Eulerâ€™s method to simulate the flight of a frisbee in
-	 * two dimensions, distance and height (x and y, respectively).
-	 *
-	 */
-	public static void simulate(double y0, double theta, double v0,
-			double alpha, double deltaT)
-	{
-		//Calculation of the lift coefficient using the relationship given
-		//by S. A. Hummel.
-		double cl = CL0 + CLA*alpha*Math.PI/180;
-		//Calculation of the drag coefficient (for Prantlâ€™s relationship)
-		//using the relationship given by S. A. Hummel.
-		double cd = CD0 + CDA*Math.pow((alpha-ALPHA0)*Math.PI/180,2);
-		//Initial position x = 0.
-		x = 0;
-		//Initial position y = y0.
-		y = y0;
-		//Initial x velocity vx = vx0.
-		vx = v0 * Math.cos(theta);
-		//Initial y velocity vy = vy0.
-		vy = v0 * Math.sin(theta);
-		try{
-			//A PrintWriter object to write the output to a spreadsheet.
-			PrintWriter pw = new PrintWriter(new BufferedWriter
-					(new FileWriter("frisbee " + v0 + "m,s " + theta + "degrees.csv")));
-			//A loop index to monitor the simulation steps.
-			int k = 0;
-			//A while loop that performs iterations until the y position
-			//reaches zero (i.e. the frisbee hits the ground).
-			while(y>0){
-				//The change in velocity in the y direction obtained setting the
-				//net force equal to the sum of the gravitational force and the
-				//lift force and solving for delta v.
-				double deltavy = (RHO*Math.pow(vx,2)*AREA*cl/2/m+g)*deltaT;
-				//The change in velocity in the x direction, obtained by
-				//solving the force equation for delta v. (The only force
-				//present is the drag force).
-				double deltavx = -RHO*Math.pow(vx,2)*AREA*cd*deltaT;
-				//The new positions and velocities are calculated using
-				//simple introductory mechanics.
-				vx = vx + deltavx;
-				vy = vy + deltavy;
-				x = x + vx*deltaT;
-				y = y + vy*deltaT;
-				//Only the output from every tenth iteration will be sent
-				//to the spreadsheet so as to decrease the number of data points.
-				if(k % 10 == 0){
-					pw.print(x + "," + y + "," + vx);
-					pw.println();
-					pw.flush();
+
+
+
+		/**
+		 * A method that uses Euler’s method to simulate the flight of a frisbee in
+		 * two dimensions, distance and height (x and y, respectively).
+		 *
+		 */
+		public static void simulate(double y0, double vx0, double vy0,
+				double alpha, double deltaT)
+		{
+			//Calculation of the lift coefficient using the relationship given
+			//by S. A. Hummel.
+			double cl = CL0 + CLA*alpha*Math.PI/180;
+			//Calculation of the drag coefficient (for Prantl’s relationship)
+			//using the relationship given by S. A. Hummel.
+			double cd = CD0 + CDA*Math.pow((alpha-ALPHA0)*Math.PI/180,2);
+			//Initial position x = 0.
+			x = 0;
+			//Initial position y = y0.
+			y = y0;
+			//Initial x velocity vx = vx0.
+			vx = vx0;
+			//Initial y velocity vy = vy0.
+			vy = vy0;
+			try{
+				//A PrintWriter object to write the output to a spreadsheet.
+				PrintWriter pw = new PrintWriter(new BufferedWriter
+						(new FileWriter("frisbee.csv")));
+				//A loop index to monitor the simulation steps.
+				int k = 0;
+				//A while loop that performs iterations until the y position
+				//reaches zero (i.e. the frisbee hits the ground).
+				while(y>0){
+					//The change in velocity in the y direction obtained setting the
+					//net force equal to the sum of the gravitational force and the
+					//lift force and solving for delta v.
+					double deltavy = (RHO*Math.pow(vx,2)*AREA*cl/2/m+g)*deltaT;
+					//The change in velocity in the x direction, obtained by
+					//solving the force equation for delta v. (The only force
+					//present is the drag force).
+					double deltavx = -RHO*Math.pow(vx,2)*AREA*cd*deltaT;
+					//The new positions and velocities are calculated using
+					//simple introductory mechanics.
+					vx = vx + deltavx;
+					vy = vy + deltavy;
+					x = x + vx*deltaT;
+					y = y + vy*deltaT;
+					//Only the output from every tenth iteration will be sent
+					//to the spreadsheet so as to decrease the number of data points.
+					if(k%10 == 0){
+						pw.print(x + "," + y + "," + vx);
+						pw.println();
+						pw.flush();
+					}
+					k++;
+					System.out.println(y);
 				}
-				k++;
+				pw.close();
 			}
-			pw.close();
+			catch(Exception e){
+				System.out.println("Error, file frisbee.csv is in use.");}
 		}
-		catch(Exception e){
-			System.out.println("Error, file frisbee.csv is in use.");}
 	}
-}
